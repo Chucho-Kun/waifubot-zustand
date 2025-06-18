afb2351bd460010e2bdbabe56bf739211665c722
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-17 19:44:44 -0600

    react installed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+     <div className="text-3xl text-center bg-pink-700 p-10 text-white font-black">WAIFUBOT WIKIPEDIA</div>
+     <div className="flex">
+        <div className=" text-center border-blue-800 shadow-2xl items-center justify-center bg-pink-100 w-1/2 flex p-6">CONTENIDO</div>
+        <div className=" text-center border-blue-800 shadow-2xl items-center justify-center bg-pink-100 w-1/2 flex p-6">MENU</div>
+     </div>
+    </>
+  )
+}
+
+export default App
